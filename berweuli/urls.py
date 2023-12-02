a1a69435da63ac9e13c75f6bf5f22825a3947619
serdar sayın <serdarsayin61@gmail.com>
2023-12-02 14:23:52 +0300

    Template düzeltmeler yapıldı.

--- berweuli/urls.py
+++ berweuli/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from berweuli.views import menu, menu_icerik
 
 urlpatterns = [
-    path('menu', menu, name='menu'),
-    path('menu_icerik', menu_icerik, name='menu_icerik')
+    path('menu', menu_icerik, name='menu')
 ]
--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from berweuli.views.menu_icerik import set_language
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('berweuli.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = [
+    *i18n_patterns(*urlpatterns, prefix_default_language=False),
+    path("set_language/<str:language>", set_language, name="set-language"),
+]
