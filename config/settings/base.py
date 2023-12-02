be04482edf7f5757e5cabce28718ee00eda02684
serdar sayın <serdarsayin61@gmail.com>
2023-12-02 13:17:56 +0300

    settings dosyası parçalandı

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,15 +12,17 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+import environ
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
-SECRET_KEY = '(+9&k#6*!x6giu*wx=1aw@*_bz+k$u+pxxstgb61j*2v8igpn#'
+SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 
@@ -66,28 +68,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+
+
 
 
 
